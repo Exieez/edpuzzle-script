32afa302edc88277c4f07e38988a8523a64534c2
Exie  <71118189+Exieez@users.noreply.github.com>
2024-04-11 21:56:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -182,7 +182,7 @@ var validKeys = [
     {
         key: "VELOH-KJDSFKJDSKFLJDKFS",
         expiration: new Date(new Date().getTime() + 5 * 60 * 1000).toISOString()
-    }
+    },
     {
         key: "VELOH-SHOWCASE",
         expiration: new Date(new Date().getTime() + 5 * 60 * 1000).toISOString()
